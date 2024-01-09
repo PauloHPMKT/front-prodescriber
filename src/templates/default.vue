91ba08d2f7863eed222b3a447a975cf3c5ce0537
Paulo Sergio <pauloserg861@gmail.com>
2024-01-09 19:37:53 -0300

    feat: add template for page structure

---
+++ src/templates/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="base_structure">
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.base_structure {
+  min-height: 40vh;
+  margin: 100px 0;
+  padding: 0 10%;
+}
+</style>

30f5494aa7372bfc50d93f02c8001018b8e7fba9
Paulo Sergio <pauloserg861@gmail.com>
2024-01-09 18:43:32 -0300

    feat: implement main-header in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainHeader from "../components/Header/index.vue";
+</script>
 
 <template>
-  <div>
-    <h1>Home</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-      voluptatum, quibusdam, quos, voluptatibus quod quia voluptatem consequatur
-      quae doloribus quas voluptates. Quisquam, quibusdam, quos, voluptatibus
-      quod quia voluptatem consequatur quae doloribus quas voluptates.
-    </p>
+  <div class="home_view">
+    <main>
+      <main-header />
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatum, quibusdam, quos, voluptatibus quod quia voluptatem
+        consequatur quae doloribus quas voluptates. Quisquam, quibusdam, quos,
+        voluptatibus quod quia voluptatem consequatur quae doloribus quas
+        voluptates.
+      </p>
+    </main>
   </div>
 </template>
+
+<style scoped lang="scss">
+.home_view {
+  margin: 0 auto;
+  height: 100vh;
+}
+</style>

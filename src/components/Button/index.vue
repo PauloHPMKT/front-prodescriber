e3e09932a8d636f7fa5192d37924df8af54887c3
Paulo Sergio <pauloserg861@gmail.com>
2024-01-09 19:33:23 -0300

    feat: create main-button component

---
+++ src/components/Button/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const emit = defineEmits(["action"]);
+
+const action = () => {
+  emit("action");
+};
+</script>
+
+<template>
+  <button @click="action">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  padding: 0 16px;
+  height: 35px;
+  border-radius: 6px;
+  border: 1px solid transparent;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+}
+</style>

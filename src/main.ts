9abbbfb9781f759a5fd02dd9fd5a91b8c043c076
Paulo Sergio <pauloserg861@gmail.com>
2024-01-09 18:19:07 -0300

    feat: instanciate routes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./style.css";
+import router from "./routers";
 
 const app = createApp(App);
+app.use(router);
 app.mount("#app");

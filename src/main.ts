8a4ff8d99f46b153e53a1197d241e18f8399c4e8
Paulo Sergio <pauloserg861@gmail.com>
2024-01-09 20:31:11 -0300

    feat: instanciate lottiefiles

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from "./App.vue";
 
 import "./assets/styles/main.scss";
 import router from "./routers";
+import Vue3Lottie from "vue3-lottie";
 
 const app = createApp(App);
 app.use(router);
+app.use(Vue3Lottie, { name: "Vue3Lottie" });
 app.mount("#app");

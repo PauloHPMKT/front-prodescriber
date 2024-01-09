7687bd0f3fe158f9e89c3bab03ff756be9f6c885
Paulo Sergio <pauloserg861@gmail.com>
2024-01-09 20:45:57 -0300

    feat: create component for Load spinner

---
+++ src/components/Loaders/LoadSpinner.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const props = defineProps({
+  loader_description: { type: String },
+});
+</script>
+
+<template>
+  <div class="loader-container">
+    <div class="loader"></div>
+    <p>{{ loader_description.value }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.loader-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .loader {
+    width: 65px;
+    height: 65px;
+    border: 5px solid #fff;
+    border-top: 5px solid #121212;
+    filter: drop-shadow(0 0 5px #ffffff33);
+    border-radius: 50%;
+    animation: roll 1s linear infinite;
+    text-align: center;
+  }
+
+  p {
+    margin-top: 30px;
+    color: #f0f0f0;
+  }
+
+  @keyframes roll {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+}
+</style>

90342756215079677f09757c3247c7b8169006f6
Paulo Sergio <pauloserg861@gmail.com>
2024-01-16 19:02:49 -0300

    feat: create revealler password

---
+++ src/components/Icons/ReveallerPassword.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import defaultIcon from "./defaultIcon.vue";
+import type { ReveallerPasswordProps } from "../../types/interfaces";
+
+defineProps<ReveallerPasswordProps>();
+
+const emit = defineEmits(["toggleRevealer"]);
+
+const toggleRevealer = () => {
+  emit("toggleRevealer");
+};
+</script>
+
+<template>
+  <span @click="toggleRevealer" style="cursor: pointer">
+    <default-icon :name="'tabler:eye'" v-if="is_password" class="centralize" />
+    <default-icon :name="'tabler:eye-off'" v-else class="centralize" />
+  </span>
+</template>
+
+<style scoped lang="scss">
+.centralize {
+  display: flex;
+  align-items: center;
+}
+</style>

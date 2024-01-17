b3bdefad30dca6240c829af99495d695f9e0ebb5
Paulo Sergio <pauloserg861@gmail.com>
2024-01-17 09:21:08 -0300

    feat: create page content and structure components

---
+++ src/templates/content.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <transition name="fade">
+    <router-view></router-view>
+  </transition>
+</template>
+
+<style scoped lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/templates/pageStructure.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Content from "./content.vue";
+</script>
+
+<template>
+  <aside>sidebar</aside>
+  <div>
+    <header>header</header>
+    <main>
+      <Content />
+    </main>
+    <footer>footer</footer>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>

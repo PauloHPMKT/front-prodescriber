343f38af5876b2565f38c81ff643440904143353
Paulo Sergio <pauloserg861@gmail.com>
2024-01-17 11:43:58 -0300

    feat: create pageStructure component

--- src/templates/pageStructure.vue
+++ src/templates/pageStructure.vue
@@ -1,16 +1,47 @@
 <script setup lang="ts">
+import Sidebar from "../components/Sidebar/index.vue";
 import Content from "./content.vue";
 </script>
 
 <template>
-  <aside>sidebar</aside>
-  <div>
-    <header>header</header>
-    <main>
-      <Content />
-    </main>
-    <footer>footer</footer>
+  <div class="structure">
+    <sidebar />
+    <div class="container">
+      <header class="user_header">
+        <div>
+          <span>Bem vindo(a) Nome do usuario logado</span>
+        </div>
+        <div class="user_info">
+          <p>Nome do usuário</p>
+          <p>Logout</p>
+        </div>
+      </header>
+      <main>
+        <Content />
+      </main>
+      <footer>footer</footer>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.structure {
+  display: flex;
+
+  .container {
+    width: 100%;
+    height: 100vh;
+    background-color: red;
+
+    .user_header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 10px 10px 20px 16%;
+      background-color: blue;
+      height: 10vh;
+      border-bottom: 1px solid #121212;
+    }
+  }
+}
+</style>

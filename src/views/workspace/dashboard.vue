8be9ee89ede9b29de7ecb32c80e548aaae6d9fae
Paulo Sergio <pauloserg861@gmail.com>
2024-01-17 11:42:58 -0300

    feat: create users page structure

---
+++ src/views/workspace/dashboard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="dashboard_container">
+    <h1>Dashboard</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.dashboard_container {
+  padding: 10px 10px 20px 16%;
+}
+</style>
---
+++ src/views/workspace/users.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="users_container">
+    <h1>User</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.users_container {
+  padding: 10px 10px 20px 16%;
+}
+</style>

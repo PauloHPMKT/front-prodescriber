0a06469898e7193cf42eb13f08426d8760eb5d99
Paulo Sergio <pauloserg861@gmail.com>
2024-01-16 21:46:13 -0300

    feat: add styles for scrollbar

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,26 @@
   <router-view></router-view>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+/* .fade-enter-active,
+.fade-leave-active {
+  transition: 400ms ease all;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+  max-width: 100%;
+} */
+/* --scrollbar */
+::-webkit-scrollbar-track {
+  background-color: #f4f4f4;
+}
+::-webkit-scrollbar {
+  width: 8px;
+  background: #444849;
+}
+::-webkit-scrollbar-thumb {
+  background: #dad7d7;
+}
+</style>

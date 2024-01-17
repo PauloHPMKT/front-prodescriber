e7e5b888aea14783c50e50b91d4d3d9d69255d85
Paulo Sergio <pauloserg861@gmail.com>
2024-01-17 11:44:33 -0300

    feat: create sidebar

---
+++ src/components/Sidebar/SidebarPlugins.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Aqui ficarão os links</h1>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/Sidebar/index.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import SidebarPlugins from "./SidebarPlugins.vue";
+import logo from "../../assets/img/logo_prodescriber.png";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const navigateToDashboard = () => {
+  router.push({ name: "dashboard" });
+};
+</script>
+
+<template>
+  <aside>
+    <div @click="navigateToDashboard" class="image_container">
+      <img :src="logo" alt="Logo ProDescriber" />
+    </div>
+    <nav>
+      lista de menu
+      <sidebar-plugins />
+    </nav>
+  </aside>
+</template>
+
+<style scoped lang="scss">
+aside {
+  width: 15%;
+  background-color: #121212;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  .image_container {
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    img {
+      width: 80%;
+    }
+  }
+
+  nav {
+    background-color: red;
+  }
+}
+</style>

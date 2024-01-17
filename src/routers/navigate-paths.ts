1cc9cf85c0451dae4fc91ca5d61505785f372754
Paulo Sergio <pauloserg861@gmail.com>
2024-01-17 16:28:58 -0300

    feat: add navigation paths

---
+++ src/routers/navigate-paths.ts
@@ -0,0 +1,16 @@
+import { NavigatePathsProps } from "../types/interfaces";
+
+export const navigatePaths: NavigatePathsProps[] = [
+  {
+    id: 0,
+    router: "/app/dashboard",
+    icon: "chart-combo-stacked",
+    description: "Dashboard",
+  },
+  {
+    id: 1,
+    router: "/app/users",
+    icon: "user-multiple",
+    description: "Usuários",
+  },
+];

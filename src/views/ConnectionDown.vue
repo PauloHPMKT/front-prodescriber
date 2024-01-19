61e9c81c5d66395ce2336b1bb8d1c6f15e878e8f
Paulo Sergio <pauloserg861@gmail.com>
2024-01-19 19:45:37 -0300

    feat: create disconnected page

---
+++ src/views/ConnectionDown.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import disconnected from "../assets/lottie_animations/disconnected.json";
+</script>
+
+<template>
+  <div class="container">
+    <div class="content">
+      <figure>
+        <Vue3Lottie :animationData="disconnected" :height="220" :width="220" />
+      </figure>
+      <h1>Ops! Parece que você está sem conexão com a internet.</h1>
+      <p>Verifique sua conexão e tente novamente.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .content {
+    text-align: center;
+
+    figure {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 300px;
+    }
+  }
+}
+</style>

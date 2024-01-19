296bd4fb84ef6ee4f69850ae70f60bc27b0ea49a
Paulo Sergio <pauloserg861@gmail.com>
2024-01-18 21:07:32 -0300

    feat: add pinia instance

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 
 import "./assets/styles/main.scss";
 import router from "./routers";
 import Vue3Lottie from "vue3-lottie";
 
+const pinia = createPinia();
 const app = createApp(App);
+
+app.use(pinia);
 app.use(router);
 app.use(Vue3Lottie, { name: "Vue3Lottie" });
 app.mount("#app");

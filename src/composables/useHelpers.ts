f29e29f16844193d46c2683d98eec384115ba8e5
Paulo Sergio <pauloserg861@gmail.com>
2024-01-19 17:06:31 -0300

    feat: add helper to format date time

--- src/composables/useHelpers.ts
+++ src/composables/useHelpers.ts
@@ -5,7 +5,25 @@ export const useHelpers = () => {
     });
   }
 
+  function truncate(text: string): string {
+    return text.split(" ").slice(0, 2).toString().replace(",", " ");
+  }
+
+  function greetings(): string {
+    const date = new Date();
+    const hour = date.getHours();
+    let greeting = "";
+
+    if (hour >= 0 && hour < 12) greeting = "Bom dia";
+    else if (hour >= 12 && hour < 18) greeting = "Boa tarde";
+    else greeting = "Boa noite";
+
+    return greeting;
+  }
+
   return {
     removeMultipleItems,
+    truncate,
+    greetings,
   };
 };

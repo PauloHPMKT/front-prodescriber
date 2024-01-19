a43bd046d4beb4ecb4a99da9ff410a732807bac1
Paulo Sergio <pauloserg861@gmail.com>
2024-01-18 21:41:19 -0300

    feat: add helper to create current date formated

--- src/composables/useOnMounted.ts
+++ src/composables/useOnMounted.ts
@@ -7,7 +7,20 @@ export const useOnMounted = () => {
     }
   }
 
+  function dateTimeFormated(): string {
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
+    const today = new Date();
+    const date = Intl.DateTimeFormat("pt-BR", options).format(today);
+    return date;
+  }
+
   return {
     removeFromStorageOnLoad,
+    dateTimeFormated,
   };
 };

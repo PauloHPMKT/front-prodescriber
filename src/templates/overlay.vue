5756aac4f8a05f71def99283f0c0ee2c485672a6
Paulo Sergio <pauloserg861@gmail.com>
2024-01-21 08:49:05 -0300

    feat: create ovelay template

---
+++ src/templates/overlay.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="overlay">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 300;
+  background-color: rgba(18, 18, 18, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>

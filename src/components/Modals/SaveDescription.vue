822fd327943c85239ada57e462eb3ad2b80a768d
Paulo Sergio <pauloserg861@gmail.com>
2024-01-21 08:51:52 -0300

    refactor: add overlay template

--- src/components/Modals/SaveDescription.vue
+++ src/components/Modals/SaveDescription.vue
@@ -2,6 +2,7 @@
 import { onMounted } from "vue";
 import Description from "../Description/index.vue";
 import { DescriptionProps } from "../../types/interfaces";
+import overlay from "../../templates/overlay.vue";
 
 const props = defineProps<DescriptionProps>();
 const emit = defineEmits(["hideDescriptionModal"]);
@@ -22,7 +23,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="overlay">
+  <overlay>
     <description
       :item="props.item"
       :result="props.result"
@@ -31,55 +32,42 @@ onMounted(() => {
       <button>Sim</button>
       <button @click="leaveDescription">Cancelar</button>
     </description>
-  </div>
+  </overlay>
 </template>
 
 <style scoped lang="scss">
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 300;
-  background-color: rgba(18, 18, 18, 0.6);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.container_modal {
+  position: relative;
+  padding: 50px;
+  background-color: #fff;
+  border-radius: 6px;
+  width: 700px;
+  max-height: 80%;
+  overflow: auto;
 
-  .container_modal {
-    position: relative;
-    padding: 50px;
-    background-color: #fff;
+  .clipboard {
+    position: absolute;
+    top: 20px;
+    right: 20px;
     border-radius: 6px;
-    width: 700px;
-    max-height: 80%;
-    overflow: auto;
+    width: 36px;
+    height: 36px;
+    padding: 5px;
+    color: #121212;
+    font-size: 1.5rem;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
 
-    .clipboard {
-      position: absolute;
-      top: 20px;
-      right: 20px;
-      border-radius: 6px;
-      width: 36px;
-      height: 36px;
-      padding: 5px;
-      color: #121212;
-      font-size: 1.5rem;
-      cursor: pointer;
-      transition: all 0.3s ease-in-out;
-
-      &:hover {
-        background-color: #121212;
-        color: #fff;
-      }
+    &:hover {
+      background-color: #121212;
+      color: #fff;
     }
+  }
 
-    .item,
-    .description_content {
-      color: #121212;
-      margin-bottom: 20px;
-    }
+  .item,
+  .description_content {
+    color: #121212;
+    margin-bottom: 20px;
   }
 }
 </style>

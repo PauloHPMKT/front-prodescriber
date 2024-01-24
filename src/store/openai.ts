6c3dff7e7ec73c1cca9b18efaa51133790a43228
Paulo Sergio <pauloserg861@gmail.com>
2024-01-24 20:41:06 -0300

    feat: create openai store

---
+++ src/store/openai.ts
@@ -0,0 +1,26 @@
+import { defineStore } from "pinia";
+import { Openai } from "../types/openai";
+import { AxiosResponse } from "axios";
+import openaiService from "../services/openai.service";
+
+export const useOpenAIStore = defineStore("openai", {
+  state: () => ({
+    descriptionContent: {
+      item: "",
+      prompt: "",
+      result: "",
+    } as Openai.ChatResponse,
+  }),
+  actions: {
+    async createOpenaiDescription(
+      request: Openai.ChatRequest | object
+    ): Promise<AxiosResponse<any, any>> {
+      const description = await openaiService.createDescription(request);
+      return description;
+    },
+    storeDescriptionContent(content: Openai.ChatResponse) {
+      this.$state.descriptionContent = content;
+      console.log(this.$state.descriptionContent);
+    },
+  },
+});

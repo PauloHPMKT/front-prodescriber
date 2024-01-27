4d74cd10c2f6ec6d914b41fa585aa2d1acaa3ffa
Paulo Sergio <pauloserg861@gmail.com>
2024-01-27 16:24:58 -0300

    feat: add formsValidation

--- src/composables/useValidation.ts
+++ src/composables/useValidation.ts
@@ -8,7 +8,24 @@ export const useValidation = () => {
 
   function required(val: string): boolean | string {
     if (val === "") {
-      return "Campo de decrição não pode ser vazio!";
+      return `Campo de decrição não pode ser vazio!`;
+    }
+    return true;
+  }
+
+  function formsValidation<T extends Record<string, any>>(
+    data: T
+  ): boolean | string {
+    const requiredFields = Object.keys(data) as Array<keyof T>;
+    const allFieldsVerify = requiredFields.every((field) => !data[field]);
+    if (allFieldsVerify) {
+      return `preencha todos os campos corretamente!`;
+    }
+    for (const field of requiredFields) {
+      if (!data[field]) {
+        const typeField = field === "password" ? "senha" : field;
+        return `preencha o campo ${String(typeField)}!`;
+      }
     }
     return true;
   }
@@ -16,5 +33,6 @@ export const useValidation = () => {
   return {
     minLength,
     required,
+    formsValidation,
   };
 };

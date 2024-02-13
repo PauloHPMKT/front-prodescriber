8e6dd7c7f348a70ad9583f3db35dfa5a401a30c1
Paulo Sergio <pauloserg861@gmail.com>
2024-02-13 14:18:51 -0300

    refactor: reorder user properties types

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,9 +1,9 @@
 export interface User {
   _id?: string | null;
-  name: string | null;
+  username: string | null;
   email: string | null;
   password: string | null | undefined;
-  company: string | null;
-  role: "admin" | "user" | null;
-  role_gpt_generate: string;
+  avatar: string | null;
+  role: "admin" | "user";
+  role_system: "user" | "system";
 }

d308d91fa5acef71d436f171b46accd1e763fd20
Paulo Sergio <pauloserg861@gmail.com>
2024-02-13 14:28:29 -0300

    feat: add toolServices list

---
+++ src/components/ToolService/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+import { NavigatePathsProps } from "../../types/interfaces";
+import { useRouter } from "vue-router";
+
+defineProps<NavigatePathsProps>();
+
+const redirect = useRouter();
+
+const toRouter = (path: string) => {
+  redirect.push(path);
+};
+</script>
+
+<template>
+  <div
+    @click="toRouter(router)"
+    class="shadow-md bg-white w-[24.2%] rounded-lg p-6 h-[13rem] border-2 border-white hover:border-slate-300"
+  >
+    <header class="flex items-start mb-4">
+      <div class="bg-slate-300 p-2 rounded">
+        <Icon :icon="`tabler:${icon}`" class="text-slate-800" />
+      </div>
+      <h3 class="ml-3 font-semibold text-zinc-900">{{ description }}</h3>
+    </header>
+    <div>
+      <p>{{ content }}</p>
+    </div>
+  </div>
+</template>

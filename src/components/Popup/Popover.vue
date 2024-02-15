a6a5da70f78ce69f1fcc4087bb523afa4a60c287
Paulo Sergio <pauloserg861@gmail.com>
2024-02-15 10:16:10 -0300

    feat: add popover component

---
+++ src/components/Popup/Popover.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface PopoverProps {
+  message: string;
+}
+
+defineProps<PopoverProps>();
+</script>
+
+<template>
+  <div class="bg-slate-800 p-2 rounded-[6px] shadow-lg min-w-56 text-slate-400">
+    {{ message }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.jumping {
+  animation: jumpAnimation 0.5s ease-in-out;
+}
+
+@keyframes jumpAnimation {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+.hidden {
+  display: none;
+}
+</style>

fd7b8b74b766d9004705a6fcbbb05b16e5d6e217
Paulo Sergio <pauloserg861@gmail.com>
2024-03-07 23:45:16 -0300

    feat: create general buttons

---
+++ src/components/Button/GeneralButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface GeneralButtonProps {
+  action: () => void;
+}
+
+defineEmits<GeneralButtonProps>();
+</script>
+
+<template>
+  <button class="bg-black text-white px-4 py-2 rounded-md" @click="action">
+    <slot></slot>
+  </button>
+</template>

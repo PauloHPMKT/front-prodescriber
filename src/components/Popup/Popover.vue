a50a4b244c94b779bca03ae7133fec0bbbeb4009
Paulo Sergio <pauloserg861@gmail.com>
2024-03-09 11:40:38 -0300

    feat: add close to popover component

--- src/components/Popup/Popover.vue
+++ src/components/Popup/Popover.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 interface PopoverProps {
   message: string;
 }
 
 defineProps<PopoverProps>();
+
+const isVisible = ref<boolean>(true);
+
+const closePopoveer = () => {
+  isVisible.value = false;
+};
 </script>
 
 <template>
   <div class="bg-slate-800 p-2 rounded-[6px] shadow-lg min-w-56 text-slate-400">
-    {{ message }}
+    <div @click="closePopoveer" class="underline cursor-pointer border-b-2 border-slate-900 pb-5 pt-3">
+      Clique aqui para fechar
+    </div>
+    <div class="mt-3">
+      {{ message }}
+    </div>
   </div>
 </template>
 

8ae11c0df7dce1b6cef105bc4b3910d50bc8ffe9
Paulo Sergio <pauloserg861@gmail.com>
2024-03-09 11:41:37 -0300

    feat: create PageTitle component

---
+++ src/components/PageTitle/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+  <div class="flex flex-col items-start my-5">
+    <h2 class="font-semibold text-[1.5rem]">{{ title }}</h2>
+  </div>
+</template>

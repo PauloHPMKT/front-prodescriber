f20629603a9b9bdcc128065ece527a7c94dd8714
Paulo Sergio <pauloserg861@gmail.com>
2024-03-09 18:16:10 -0300

    refactor: create Hero and implement to Home page

---
+++ src/components/site/Hero.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue"; 
+import { useRouter } from 'vue-router';
+import MainButton from "@/components/Button/index.vue";
+
+const router = useRouter();
+
+const registration = () => {
+  if (localStorage.getItem("access_token")) {
+    router.push({ name: "dashboard" });
+    return;
+  }
+  return router.push({ name: "register" });
+};
+</script>
+
+<template>
+  <div class="w-[80%] m-auto text-center py-10">
+    <h1 class="text-4xl font-semibold leading-tight mb-9">
+      <span class="text-slate-300 font-medium">ProDescriber</span>, seu
+      parceiro confiável para criação de descrições de produtos!
+    </h1>
+    <p class="leading-normal text-lg px-20 pb-10">
+      Crei descrições de alta qualidade e totalmente SEO-friendly para
+      seu produto e impulsione suas vendas. Experimente já o
+      ProDescriber!
+    </p>
+    <div class="mt-8">
+      <MainButton @click="registration" class="bg-button text-white">
+        Comece agora Gratuitamente
+        <Icon :icon="'tabler:wand'" />
+      </MainButton>
+    </div>
+    <!-- <div id="demo" v-if="showDemo">
+      <transition name="slide-fade">
+        <main-button
+          v-if="show === false"
+          @click="show = !show"
+          class="bg-button text-white"
+        >
+          Comece agora Gratuitamente
+          <Icon :icon="'tabler:wand'" />
+        </main-button>
+        <form-description v-else @submit="submitDescription" />
+      </transition>
+    </div>
+    <div v-else>
+      <div v-if="loading" style="text-align: center">
+        <div class="loader_container">
+          <load-spinner
+            :loader_description="'Aguarde enquanto geramos a sua descrição perfeita!'"
+          />
+        </div>
+      </div>
+      <description v-else>
+        <breadcrumb-buttons
+          :buttons="breadcrumbButtons"
+          @actions="triggerActions"
+        />
+      </description>
+    </div> -->
+  </div>
+</template>
+
+<style scoped lang="scss">
+.bg-button {
+  position: absolute;
+  transform: translate(-50%);
+  z-index: 2000;
+  padding-top: 7px;
+  padding-bottom: 7px;
+  border: 1px solid #c5c5c51c;
+  background-image: linear-gradient(45deg, #202020, #292727);
+  transition: all 0.3s ease-out;
+
+  &:hover {
+    filter: drop-shadow(0 0 0.4rem #028a616e);
+    cursor: pointer;
+  }
+
+  svg {
+    margin-left: 10px;
+    font-size: 1.2rem;
+  }
+}
+
+/* .content {
+  .slide-fade-enter-active {
+    transition: all 0.4s ease-out;
+  }
+
+  .slide-fade-leave-active {
+    transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+  }
+
+  .slide-fade-enter-from,
+  .slide-fade-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+  }
+
+  .loader_container {
+    margin: 30px auto;
+    padding: 30px;
+    width: 600px;
+    border-radius: 8px;
+  }
+} */
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,9 @@ import { Icon } from "@iconify/vue";
 //components
 import MainHeader from "../components/Header/index.vue";
 import MainButton from "../components/Button/index.vue";
-import FormDescription from "../components/Forms/FormDescription.vue";
-import LoadSpinner from "../components/Loaders/LoadSpinner.vue";
 import BreadcrumbButtons from "../components/BreadcrumbButtons/index.vue";
 import Default from "../templates/default.vue";
-import Description from "../components/Description/index.vue";
+import Hero from "@/components/site/Hero.vue";
 import About from "../components/site/About.vue";
 //externals
 import { Breadcrumb } from "../types/interfaces";
@@ -34,12 +32,6 @@ const { chatRequest } = useOpenai();
 const { minLength, required } = useValidation();
 const { removeStoreOnLoad } = useOnMounted();
 
-const showDemo = ref(true);
-const show = ref(false);
-const loading = ref(false);
-const product = ref("");
-const result = ref("");
-const message = ref("");
 const breadcrumbButtons = ref([
   {
     label: "Gostei",
@@ -75,14 +67,6 @@ const triggerActions = (actions: string) => {
     default:
       break;
   }
-};
-
-const registration = () => {
-  if (localStorage.getItem("access_token")) {
-    router.push({ name: "dashboard" });
-    return;
-  }
-  return router.push({ name: "register" });
 };
 
 const saveDescription = (token: string | null) => {
@@ -148,51 +132,7 @@ onMounted(() => {
       <div class="banner_bg"></div>
       <main>
         <default>
-          <div class="content">
-            <h1 class="text-2xl">
-              <span class="text-slate-300 font-medium">ProDescriber</span>, seu
-              parceiro confiável para criação de descrições de produtos!
-            </h1>
-            <p>
-              Crei descrições de alta qualidade e totalmente SEO-friendly para
-              seu produto e impulsione suas vendas. Experimente já o
-              ProDescriber!
-            </p>
-            <div class="mt-8">
-              <main-button @click="registration" class="bg-button text-white">
-                Comece agora Gratuitamente
-                <Icon :icon="'tabler:wand'" />
-              </main-button>
-            </div>
-            <!-- <div id="demo" v-if="showDemo">
-              <transition name="slide-fade">
-                <main-button
-                  v-if="show === false"
-                  @click="show = !show"
-                  class="bg-button text-white"
-                >
-                  Comece agora Gratuitamente
-                  <Icon :icon="'tabler:wand'" />
-                </main-button>
-                <form-description v-else @submit="submitDescription" />
-              </transition>
-            </div>
-            <div v-else>
-              <div v-if="loading" style="text-align: center">
-                <div class="loader_container">
-                  <load-spinner
-                    :loader_description="'Aguarde enquanto geramos a sua descrição perfeita!'"
-                  />
-                </div>
-              </div>
-              <description v-else>
-                <breadcrumb-buttons
-                  :buttons="breadcrumbButtons"
-                  @actions="triggerActions"
-                />
-              </description>
-            </div> -->
-          </div>
+          <Hero />
         </default>
         <default>
           <About />
@@ -217,71 +157,10 @@ onMounted(() => {
     position: absolute;
     top: -15%;
     left: 49.7%;
+    z-index: 10;
     transform: translate(-50%);
     filter: invert(100%) sepia(90%) saturate(0%) hue-rotate(125deg)
       brightness(1000%);
-  }
-  .content {
-    width: 80%;
-    margin: 0 auto;
-    padding: 40px 0;
-    text-align: center;
-
-    h1 {
-      font-size: 2.8rem;
-      font-weight: 600;
-      line-height: 1.2;
-      margin-bottom: 35px;
-      text-shadow: 0 0 1px #fff;
-    }
-
-    p {
-      line-height: 1.5;
-      font-size: 1.1rem;
-      color: #a3a3a3;
-      padding: 0 80px 40px;
-    }
-
-    .bg-button {
-      position: absolute;
-      transform: translate(-50%);
-      padding-top: 7px;
-      padding-bottom: 7px;
-      border: 1px solid #c5c5c51c;
-      background-image: linear-gradient(45deg, #202020, #292727);
-      transition: all 0.3s ease-out;
-
-      &:hover {
-        filter: drop-shadow(0 0 0.4rem #028a616e);
-        cursor: pointer;
-      }
-
-      svg {
-        margin-left: 10px;
-        font-size: 1.2rem;
-      }
-    }
-
-    .slide-fade-enter-active {
-      transition: all 0.4s ease-out;
-    }
-
-    .slide-fade-leave-active {
-      transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
-    }
-
-    .slide-fade-enter-from,
-    .slide-fade-leave-to {
-      transform: translateX(20px);
-      opacity: 0;
-    }
-
-    .loader_container {
-      margin: 30px auto;
-      padding: 30px;
-      width: 600px;
-      border-radius: 8px;
-    }
   }
 }
 </style>

818191504e8e39187cb12078169cfa226bcd79d5
Paulo Sergio <pauloserg861@gmail.com>
2024-04-04 18:42:59 -0300

    chore: install vue3-toastify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import App from "./App.vue";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import App from "./App.vue";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 
 import "./assets/styles/main.scss";
 import "./assets/styles/index.css";
 import router from "./routers";
 import Vue3Lottie from "vue3-lottie";
+import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -15,4 +16,7 @@ app.use(pinia);
 pinia.use(piniaPluginPersistedState);
 app.use(router);
 app.use(Vue3Lottie, { name: "Vue3Lottie" });
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+} as ToastContainerOptions);
 app.mount("#app");

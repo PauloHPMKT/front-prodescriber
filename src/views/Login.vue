f262db43957d1cad19cd6e1c27d0599f7f6c8915
Paulo Sergio <pauloserg861@gmail.com>
2024-04-05 19:53:53 -0300

    feat: implement loader page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,9 +6,11 @@ import InputField from "../components/Inputs/InputField.vue";
 import GeneralButton from "../components/Button/GeneralButton.vue";
 
 import { useValidation } from "../composables/useValidation";
+import { useHelpers } from "../composables/useHelpers";
 import { useToast } from "../composables/useToast";
 import { Account } from "../types/account";
 import logo from "../assets/img/logo_prodescriber.png";
+import loadingBot from "../assets/lottie_animations/loadingBot.json";
 
 import { useAuthStore } from "../store/index";
 
@@ -16,80 +18,109 @@ const router = useRouter();
 const authStore = useAuthStore();
 const { formsValidation } = useValidation();
 const { showToast } = useToast();
+const { truncateText } = useHelpers();
 
 const loginData = ref<Account.Login>({
   email: "",
   password: "",
 });
+const isLoading = ref(false);
 
 const toHomePage = () => router.push({ name: "home" });
 
 const submitLogin = async () => {
   const request = loginData.value;
   const isEmptyFields = formsValidation(request);
+  const TIME_TO_REDIRECT = 3000;
 
   if (isEmptyFields !== true) {
     showToast("error", String(isEmptyFields));
     return;
   }
-
+  
   try {
+    isLoading.value = true;
     const authLogin = await authStore.login(request);
     const { data, status } = authLogin;
     if (status === 200) {
-      const access_token = data.access_token;
-      localStorage.setItem("access_token", access_token);
       const user = data.user;
       authStore.getCurrentUser(user);
-
-      const description = localStorage.getItem("result");
-      return description
-        ? router.push({
-            name: "dashboard",
-            query: { description },
-          })
-        : router.push({ name: "dashboard" });
+      setTimeout(() => {
+        isLoading.value = false;     
+        const access_token = data.access_token;
+        localStorage.setItem("access_token", access_token);
+  
+        router.push({ name: "dashboard" })
+      }, TIME_TO_REDIRECT)
     }
   } catch (error) {
+    isLoading.value = false;
     showToast("error", "Erro ao realizar o login!");
   }
 };
 </script>
 
 <template>
-  <div class="h-screen relative flex flex-col justify-center items-center">
-    <BackTo message="Voltar para a Home" @navigateAction="toHomePage" />
-    <form
-      @submit.prevent="submitLogin"
-      class="w-[400px] flex flex-col gap-4 justify-center items-center"
-    >
-      <figure>
-        <img
-          :src="logo"
-          alt="Logo ProDescriber"
-          class="filter invert w-[250px] -mx-2"
+  <transition name="fade">
+
+    <div v-if="isLoading" class="h-screen flex justify-center items-center"> 
+      <div>
+        <Vue3Lottie              
+          :animationData="loadingBot"
+          :height="150"
+          :width="150"
+          :loop="true"
+          :speed="3"
         />
-        <figcaption>
-          <h3 class="text-gray-950 font-semibold">Área de Login</h3>
-        </figcaption>
-        <div class="flex items-center">
-          <p class="my-6 mr-1">Ainda não criou sua Workspace?</p>
-          <RouterLink :to="{ name: 'register' }" class="text-blue-600 underline"
-            >Registrar</RouterLink
-          >
-        </div>
-      </figure>
-      <div class="flex flex-col gap-5 w-full">
-        <InputField placeholder="E-mail de Usuário" v-model="loginData.email" />
-        <InputField
-          placeholder="Sua Senha"
-          :isPassword="true"
-          v-model="loginData.password"
-        />
-        <GeneralButton class="w-full h-14"> Entrar </GeneralButton>
+        <span class="text-gray-950 font-semibold text-[20px]">
+          {{ `Olá ${truncateText(String(authStore.$state.currentUser.username))}, que bom te ver por aqui!` }}
+        </span>
       </div>
-    </form>
-  </div>
+    </div>
+    <div v-else class="h-screen relative flex flex-col justify-center items-center">
+      <BackTo message="Voltar para a Home" @navigateAction="toHomePage" />
+      <form
+        @submit.prevent="submitLogin"
+        class="w-[400px] flex flex-col gap-4 justify-center items-center"
+      >
+        <figure>
+          <img
+            :src="logo"
+            alt="Logo ProDescriber"
+            class="filter invert w-[250px] -mx-2"
+          />
+          <figcaption>
+            <h3 class="text-gray-950 font-semibold">Área de Login</h3>
+          </figcaption>
+          <div class="flex items-center">
+            <p class="my-6 mr-1">Ainda não criou sua Workspace?</p>
+            <RouterLink :to="{ name: 'register' }" class="text-blue-600 underline">
+              Registrar
+            </RouterLink>
+          </div>
+        </figure>
+        <div class="flex flex-col gap-5 w-full">
+          <InputField placeholder="E-mail de Usuário" v-model="loginData.email" />
+          <InputField
+            placeholder="Sua Senha"
+            :isPassword="true"
+            v-model="loginData.password"
+          />
+          <GeneralButton class="w-full h-14"> Entrar </GeneralButton>
+        </div>
+      </form>
+    </div>
+  </transition>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>

9ad6e6d40a201cc7c3630313a3afcef74908c541
Paulo Sergio <pauloserg861@gmail.com>
2024-04-07 10:39:03 -0300

    feat: create component to page transition

---
+++ src/components/PageTransition/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import loadingBot from "../../assets/lottie_animations/loadingBot.json";
+import { useHelpers } from "../../composables/useHelpers";
+
+const { truncateText } = useHelpers();
+
+interface LoadingProps {
+  username: string;
+}
+const props = defineProps<LoadingProps>();
+
+const loadingTitle = computed(() => (
+  localStorage.getItem('access_token')
+   ? `Olá ${truncateText(String(props.username))}, que bom te ver por aqui!`
+   : 'Carregando Login...'
+));
+</script>
+
+<template>
+  <div class="h-screen flex justify-center items-center bg-white"> 
+    <div>
+      <Vue3Lottie              
+        :animationData="loadingBot"
+        :height="150"
+        :width="150"
+        :loop="true"
+        :speed="3"
+      />
+      <span class="text-gray-950 font-semibold text-[20px]">
+        {{ loadingTitle }}
+      </span>
+    </div>
+  </div>
+</template>

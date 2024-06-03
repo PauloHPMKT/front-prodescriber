7723cd324ff0353bdf263c6a3c80639f119d2a20
Paulo Sergio <pauloserg861@gmail.com>
2024-06-03 07:55:37 -0300

    style: redesign about section

--- src/components/site/About.vue
+++ src/components/site/About.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { Icon } from "@iconify/vue";
+import { ref } from "vue";
 
 const recipies = ref([
   {
@@ -22,49 +22,40 @@ const recipies = ref([
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis ipsum dicta sed, facere corrupti optio quam, quasi architecto quidem veniam inventore, debitis assumenda enim temporibus nulla qui culpa ducimus doloremque.",
   },
 ]);
+
 </script>
 
 <template>
-  <div class="about_area text-center">
+  <div class="py-12 text-center bg-gradient-to-b from-gray-200 to-gray-300">
     <div class="flex justify-center items-center">
-      <Icon icon="tabler:recharging" class="text-[30px] mt-1 mr-3" />
-      <h1 class="text-4xl">
-        <span class="text-slate-300">Melhore</span> o desempenho de seus
+      <Icon icon="tabler:recharging" class="text-[30px] mt-1 mr-3 text-slate-900" />
+      <h2 class="text-4xl text-black">
+        <span class="text-slate-900">Melhore</span> o desempenho de seus
         negócios
-      </h1>
+      </h2>
     </div>
-    <p class="py-3">
+    <p class="py-3 text-gray-800 text-[16px]">
       Descubra mais do que você já fez com a IA. Maximize sua produtividade.
       <br />Otimize seu tempo!
     </p>
     <div class="mt-20">
       <ul class="flex px-14">
         <li
-          v-for="recipe in recipies"
-          :key="recipe.title"
+          v-for="{ title, icon, description } in recipies"
+          :key="title"
           class="w-2/6 flex justify-center"
         >
           <div class="flex flex-col items-center w-80">
             <div
               class="rounded-[100%] mb-4 bg-black w-10 h-10 flex justify-center items-center"
             >
-              <Icon :icon="recipe.icon" />
+              <Icon :icon="icon" />
             </div>
-            <h2>{{ recipe.title }}</h2>
-            <p class="my-3">{{ recipe.description }}</p>
+            <h2 class="text-slate-900 ">{{ title }}</h2>
+            <p class="my-3 text-gray-800">{{ description }}</p>
           </div>
         </li>
       </ul>
     </div>
   </div>
 </template>
-
-<style scoped lang="scss">
-.about_area {
-  border-top: 2px solid #028a616e;
-  border-top-left-radius: 75px;
-  border-top-right-radius: 75px;
-  background-image: linear-gradient(180deg, #06503a6e, #12121200);
-  padding: 50px 0;
-}
-</style>
